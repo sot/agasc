3e1899e868772be57f6d5872ee2eade90bda92b1
Tom Aldcroft <taldcroft@gmail.com>
2013-05-18 22:07:00 -0400

    Add infrastructure files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+from agasc.version import version
+
+setup(name='agasc',
+      version=version,
+      description='AGASC catalog access',
+      author='Jean Connelly, Tom Aldcroft',
+      author_email='taldcroft@head.cfa.harvard.edu',
+      url='http://www.python.org/',
+      packages=['agasc'],
+      )
