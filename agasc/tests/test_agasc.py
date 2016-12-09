9900cc894f3113c991d1b6d0a43439aedbea626f
Tom Aldcroft <taldcroft@gmail.com>
2016-12-09 15:45:56 -0500

    Modernize and improve tests

--- agasc/tests/test_agasc.py
+++ agasc/tests/test_agasc.py
@@ -14,20 +14,33 @@ ACA_MAG_ERR in mp_get_agasc are limited, so this produces slightly
 different results when the faint limit filtering is applied.
 
 Comprehensive test (takes a while):
->>> import test_agasc
->>> test_agasc.test_agasc(nsample=100, agasc_file='miniagasc.h5') # local miniagasc
->>> test_agasc.test_agasc(nsample=100) # installed  (/proj/sot/ska/data/agasc/miniagasc.h5)
+>>> from test_agasc import interactive_test_agasc
+>>> interactive_test_agasc(nsample=100, agasc_file='miniagasc.h5') # local miniagasc
+>>> interactive_test_agasc(nsample=100) # installed  (/proj/sot/ska/data/agasc/miniagasc.h5)
 """
 
+from __future__ import print_function, division
 
 import os
+import re
 
 import numpy as np
 import Ska.Shell
 from astropy.io import ascii
-from astropy.coordinates import SkyCoord
+import pytest
 
 import agasc
+
+# See if we can get to ASCDS environment and mp_get_agasc
+ascrc_file = '{}/.ascrc'.format(os.environ['HOME'])
+try:
+    assert os.path.exists(ascrc_file)
+    ascds_env = Ska.Shell.getenv('source {} -r release'.format(ascrc_file), shell='tcsh')
+    assert 'ASCDS_BIN' in ascds_env
+except Exception:
+    ascds_env = None
+
+HAS_KSH = os.path.exists('/bin/ksh')  # dependency of mp_get_agascid
 
 AGASC_COL_DESCR = """
 AGASC_ID - a unique long integer used for identification.
@@ -90,12 +103,8 @@ def random_ra_dec(nsample):
 
 
 def mp_get_agasc(ra, dec, radius):
-    tcsh_cmd = 'source {}/.ascrc -r release; mp_get_agasc -r {!r} -d {!r} -w {!r}'.format(
-        os.environ['HOME'], ra, dec, radius * 1.5)
-    cmd = 'tcsh -c {!r}'.format(tcsh_cmd)
-    lines = Ska.Shell.bash(cmd)
-    i_start = [i for i, line in enumerate(lines) if line.startswith('CXC Data System')][-1]
-    lines = lines[i_start + 1:]
+    cmd = 'mp_get_agasc -r {!r} -d {!r} -w {!r}'.format(ra, dec, radius * 1.5)
+    lines = Ska.Shell.tcsh(cmd, env=ascds_env)
     dat = ascii.read(lines, Reader=ascii.NoHeader, names=AGASC_COLNAMES)
 
     ok1 = agasc.sphere_dist(ra, dec, dat['RA'], dat['DEC']) <= radius
@@ -105,49 +114,76 @@ def mp_get_agasc(ra, dec, radius):
     return dat
 
 
-def test_agasc(radius=1.4, nsample=2, agasc_file=None):
+def interactive_test_agasc(nsample=5, radius=1.4, agasc_file=None):
     ras, decs = random_ra_dec(nsample)
-    ras = np.hstack([ras, [0., 180., 0.1, 180.]])
-    decs = np.hstack([decs, [89.9, -89.9, 0.0, 0.0]])
-
     for ra, dec in zip(ras, decs):
-        print ra, dec
-
-        stars1 = agasc.get_agasc_cone(ra, dec, radius=radius, agasc_file=agasc_file)
-        stars1.sort('AGASC_ID')
-
-        stars2 = mp_get_agasc(ra, dec, radius)
-        stars2.sort('AGASC_ID')
-
-        # First make sure that the common stars are identical
-        agasc_ids = set(stars1['AGASC_ID']).intersection(set(stars2['AGASC_ID']))
-        for agasc_id in agasc_ids:
-            star1 = stars1[np.searchsorted(stars1['AGASC_ID'], agasc_id)]
-            star2 = stars2[np.searchsorted(stars2['AGASC_ID'], agasc_id)]
-            for colname in AGASC_COLNAMES:
-                if star1[colname].dtype.kind == 'f':
-                    assert np.all(np.abs(star1[colname] - star2[colname]) < 1e-4)
-                else:
-                    assert star1[colname] == star2[colname]
-
-        # Second make sure that the non-common stars are all just at the edge
-        # of the faint mag limit, due to precision loss in mp_get_agasc
-        for s1, s2 in ((stars1, stars2),
-                       (stars2, stars1)):
-            mm1 = set(s1['AGASC_ID']) - set(s2['AGASC_ID'])
-            for agasc_id in mm1:
-                idx = np.flatnonzero(s1['AGASC_ID'] == agasc_id)[0]
-                star = s1[idx]
-                bad_is_star1 = s1 is stars1
-                rad = agasc.sphere_dist(ra, dec, star['RA'], star['DEC'])
-                adj_mag = star['MAG_ACA'] - 3.0 * star['MAG_ACA_ERR'] / 100.0
-                if adj_mag < 11.5 * 0.99:
-                    # Allow for loss of precision in output of mp_get_agasc
-                    print 'Bad star', agasc_id, rad, adj_mag, bad_is_star1
-                    assert False
+        print(ra, dec)
+        _test_agasc(ra, dec, radius, agasc_file)
 
 
-def test_pm():
+ras, decs = random_ra_dec(2)
+ras = np.hstack([ras, [0., 180., 0.1, 180., 275.36476417402469]])
+decs = np.hstack([decs, [89.9, -89.9, 0.0, 0.0, 8.0999841645324135]])
+# The (275.36, 8.09) coordinate fails unless date=2000:001 due to
+# mp_get_agasc not accounting for proper motion.
+
+
+@pytest.mark.skipif('ascds_env is None')
+@pytest.mark.parametrize("ra,dec", list(zip(ras, decs)))
+def test_agasc_conesearch(ra, dec):
+    _test_agasc(ra, dec)
+
+
+def _test_agasc(ra, dec, radius=1.4, agasc_file=None):
+    stars1 = agasc.get_agasc_cone(ra, dec, radius=radius, agasc_file=agasc_file,
+                                  date='2000:001')
+    stars1.sort('AGASC_ID')
+
+    stars2 = mp_get_agasc(ra, dec, radius)
+    stars2.sort('AGASC_ID')
+
+    # First make sure that the common stars are identical
+    agasc_ids = set(stars1['AGASC_ID']).intersection(set(stars2['AGASC_ID']))
+    for agasc_id in agasc_ids:
+        star1 = stars1[np.searchsorted(stars1['AGASC_ID'], agasc_id)]
+        star2 = stars2[np.searchsorted(stars2['AGASC_ID'], agasc_id)]
+        for colname in AGASC_COLNAMES:
+            if star1[colname].dtype.kind == 'f':
+                assert np.all(np.abs(star1[colname] - star2[colname]) < 1e-4)
+            else:
+                assert star1[colname] == star2[colname]
+
+    # Second make sure that the non-common stars are all just at the edge
+    # of the faint mag limit, due to precision loss in mp_get_agasc
+    for s1, s2 in ((stars1, stars2),
+                   (stars2, stars1)):
+        mm1 = set(s1['AGASC_ID']) - set(s2['AGASC_ID'])
+        for agasc_id in mm1:
+            idx = np.flatnonzero(s1['AGASC_ID'] == agasc_id)[0]
+            star = s1[idx]
+            bad_is_star1 = s1 is stars1
+            rad = agasc.sphere_dist(ra, dec, star['RA'], star['DEC'])
+            adj_mag = star['MAG_ACA'] - 3.0 * star['MAG_ACA_ERR'] / 100.0
+            if adj_mag < 11.5 * 0.99:
+                # Allow for loss of precision in output of mp_get_agasc
+                print('Bad star', agasc_id, rad, adj_mag, bad_is_star1)
+                assert False
+
+
+def test_basic():
+    star = agasc.get_star(1180612288)  # High-PM star
+    assert np.isclose(star['RA'], 219.9053773)
+    assert np.isclose(star['DEC'], -60.8371572)
+
+    stars = agasc.get_agasc_cone(star['RA'], star['DEC'], 0.5)
+    stars.sort('MAG_ACA')
+    agasc_ids = [1180612176, 1180612296, 1180612184, 1180612288, 1180612192]
+    mags = [-0.663, -0.576, -0.373, 0.53, 0.667]
+    assert np.allclose(stars['AGASC_ID'][:5], agasc_ids)
+    assert np.allclose(stars['MAG_ACA'][:5], mags)
+
+
+def test_proper_motion():
     """
     Test that the filtering in get_agasc_cone correctly expands the initial
     search radius and then does final filtering using PM-corrected positions.
@@ -171,3 +207,36 @@ def test_pm():
     stars = agasc.get_agasc_cone(star['RA_PMCORR'], star['DEC_PMCORR'], radius, date='2017:001',
                                  pm_filter=False)
     assert len(stars) == 0
+
+
+def mp_get_agascid(agasc_id):
+    cmd = 'mp_get_agascid {!r}'.format(agasc_id)
+    lines = Ska.Shell.tcsh(cmd, env=ascds_env)
+    lines = [line for line in lines if re.match(r'^\s*\d', line)]
+    dat = ascii.read(lines, Reader=ascii.NoHeader, names=AGASC_COLNAMES)
+
+    return dat
+
+
+@pytest.mark.skipif('not HAS_KSH')
+@pytest.mark.skipif('ascds_env is None')
+def test_agasc_id(radius=0.2, npointings=2, nstar_limit=5, agasc_file=None):
+    ras, decs = random_ra_dec(npointings)
+
+    for ra, dec in zip(ras, decs):
+        print('ra, dec =', ra, dec)
+        cone_stars = agasc.get_agasc_cone(ra, dec, radius=radius, agasc_file=agasc_file)
+
+        if len(cone_stars) == 0:
+            return
+
+        cone_stars.sort('AGASC_ID')
+        for agasc_id in cone_stars['AGASC_ID'][:nstar_limit]:
+            print('  agasc_id =', agasc_id)
+            star1 = agasc.get_star(agasc_id)
+            star2 = mp_get_agascid(agasc_id)
+            for colname in AGASC_COLNAMES:
+                if star1[colname].dtype.kind == 'f':
+                    assert np.all(np.allclose(star1[colname], star2[colname]))
+                else:
+                    assert star1[colname] == star2[colname]
--- setup.py
+++ setup.py
@@ -2,11 +2,18 @@ from distutils.core import setup
 
 from agasc.version import version
 
+try:
+    from testr.setup_helper import cmdclass
+except ImportError:
+    cmdclass = {}
+
 setup(name='agasc',
       version=version,
       description='AGASC catalog access',
       author='Jean Connelly, Tom Aldcroft',
-      author_email='taldcroft@head.cfa.harvard.edu',
-      url='http://www.python.org/',
-      packages=['agasc'],
+      author_email='taldcroft@cfa.harvard.edu',
+      url='http://cxc.harvard.edu/mta/ASPECT/tool_doc/agasc',
+      packages=['agasc', 'agasc.tests'],
+      tests_require=['pytest'],
+      cmdclass=cmdclass,
       )
