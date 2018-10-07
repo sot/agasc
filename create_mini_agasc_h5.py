a6b1de813325a3ad7db3eaf40eb6e14e62f17dc5
Tom Aldcroft <taldcroft@gmail.com>
2018-10-07 06:53:50 -0400

    Exclude rare cols and include near neighbors in miniagasc

--- create_mini_agasc_h5.py
+++ create_mini_agasc_h5.py
@@ -1,4 +1,5 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
+
 import argparse
 import re
 
@@ -11,21 +12,54 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--version',
                     default='1p7',
                     help='Version (e.g. 1p6 or 1p7, default=1p7')
+parser.add_argument('--include-near-neighbors',
+                    default=True,
+                    action='store_false',
+                    help='Include near neighbor stars')
+parser.add_argument('--exclude-rarely-used-cols',
+                    default=True,
+                    action='store_false',
+                    help='Exclude columns that are rarely used in operations')
 args = parser.parse_args()
 
 num_version = re.sub(r'p', '.', args.version)
 
-example_file = '/proj/sot/ska/data/agasc{}/agasc/n0000/0001.fit'.format(args.version)
-dtype = Ska.Table.read_fits_table(example_file).dtype
-table_desc, bo = tables.descr_from_dtype(dtype)
-
 filename = 'agasc{}.h5'.format(args.version)
 print 'Reading full AGASC {} and selecting useable stars'.format(filename)
-h5 = tables.openFile(filename, mode='r')
-tbl = h5.getNode("/", 'data')
-idxs = tbl.getWhereList("(MAG_ACA - (3.0 * MAG_ACA_ERR / 100.0)) < 11.5")
-stars = tbl.readCoordinates(idxs)
+
+h5 = tables.openFile(filename)
+stars = h5.root.data[:]
 h5.close()
+
+# Filter mags
+ok = stars['MAG_ACA'] - 3.0 * stars['MAG_ACA_ERR'] / 100.0 < 11.5
+
+# Put back near-neighbor stars that got cut by above mag filter. This file
+# is made with create_near_neighbor_ids.py.
+if args.include_near_neighbors:
+    near_file = 'near_neighbor_ids_{}.fits.gz'.format(args.version)
+    near_table = Table.read(near_file, format='fits')
+    near_ids = set(near_table['near_id'])
+    print "Including {} near neighbor stars".format(len(near_ids))
+    for idx, agasc_id in enumerate(stars['AGASC_ID']):
+        if agasc_id in near_ids:
+            ok[idx] = True
+
+# Filter down to miniagasc stars
+print 'Filtering from {} to {} stars'.format(len(stars), np.count_nonzero(ok))
+stars = stars[ok]
+
+if args.exclude_rarely_used_cols:
+    print 'Excluding rarely used columns'
+    excludes = ['PLX', 'PLX_ERR', 'PLX_CATID',
+                'ACQQ1', 'ACQQ2', 'ACQQ3', 'ACQQ4', 'ACQQ5', 'ACQQ6',
+                'XREF_ID1', 'XREF_ID2', 'XREF_ID3', 'XREF_ID4', 'XREF_ID5',
+                'RSV4', 'RSV5', 'RSV6']
+    names = [name for name in stars.dtype.names if name not in excludes]
+    print 'Dtype before excluding:\n', stars.dtype
+    stars = Table([stars[name] for name in names], names=names, copy=False)
+    stars = stars.as_array()
+    print 'Dtype after excluding:\n', stars.dtype
 
 print 'Sorting on Dec and re-ordering'
 idx = np.argsort(stars['DEC'])
@@ -33,6 +67,7 @@ stars = stars.take(idx)
 
 print 'Creating miniagasc.h5 file'
 filename = 'miniagasc_{}.h5'.format(args.version)
+table_desc, bo = tables.descr_from_dtype(stars.dtype)
 minih5 = tables.openFile(filename, mode='w')
 minitbl = minih5.createTable('/', 'data', table_desc,
                              title='AGASC {}'.format(num_version))
