a65112400f9d8ed34d733138f4a03e7715703c78
Javier Gonzalez <javierggt@yahoo.com>
2021-03-09 08:35:48 -0500

    refactor task schedules to implement weekly workflow

--- setup.py
+++ setup.py
@@ -17,7 +17,12 @@ entry_points = {
 }
 
 
-data_files = [(os.path.join('share', 'agasc'), ['task_schedule.cfg'])]
+data_files = [(
+    os.path.join('share', 'agasc'),
+    ['task_schedules/task_schedule_supplement_dispositions.cfg',
+     'task_schedules/task_schedule_promote_supplement.cfg',
+     'task_schedules/task_schedule_update_supplement_rc.cfg']
+)]
 
 
 setup(name='agasc',
