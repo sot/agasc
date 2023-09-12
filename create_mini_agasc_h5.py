f9e2d23a63b9109aefa322f493ab8021684e9bfe
Tom Aldcroft <taldcroft@gmail.com>
2023-09-12 19:41:50 -0400

    Minimal modernization of create_mini_agasc_h5.py

--- create_mini_agasc_h5.py
+++ create_mini_agasc_h5.py
@@ -7,87 +7,95 @@ import numpy as np
 import tables
 from astropy.table import Table
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--version',
-                    default='1p7',
-                    help='Version (e.g. 1p6 or 1p7, default=1p7')
-parser.add_argument('--ignore-near-neighbors',
-                    action='store_true',
-                    help='Ignore near neighbor stars')
-parser.add_argument('--proseco',
-                    action='store_true',
-                    help='Create proseco_agasc')
-args = parser.parse_args()
+def get_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--version',
+                        default='1p7',
+                        help='Version (e.g. 1p6 or 1p7, default=1p7')
+    parser.add_argument('--ignore-near-neighbors',
+                        action='store_true',
+                        help='Ignore near neighbor stars')
+    parser.add_argument('--proseco',
+                        action='store_true',
+                        help='Create proseco_agasc')
+    return parser
 
-num_version = re.sub(r'p', '.', args.version)
+def main():
+    parser = get_parser()
+    args = parser.parse_args()
 
-filename = 'agasc{}.h5'.format(args.version)
-print('Reading full AGASC {} and selecting useable stars'.format(filename))
+    num_version = re.sub(r'p', '.', args.version)
 
-h5 = tables.open_file(filename)
-stars = h5.root.data[:]
-h5.close()
+    filename = 'agasc{}.h5'.format(args.version)
+    print('Reading full AGASC {} and selecting useable stars'.format(filename))
 
-# Filter mags
-ok = stars['MAG_ACA'] - 3.0 * stars['MAG_ACA_ERR'] / 100.0 < 11.5
+    with tables.open_file(filename) as h5:
+        stars = h5.root.data[:]
 
-# Put back near-neighbor stars that got cut by above mag filter. This file
-# is made with create_near_neighbor_ids.py.
-if args.proseco or not args.ignore_near_neighbors:
-    near_file = 'near_neighbor_ids_{}.fits.gz'.format(args.version)
-    near_table = Table.read(near_file, format='fits')
-    near_ids = set(near_table['near_id'])
-    print("Including {} near neighbor stars".format(len(near_ids)))
-    for idx, agasc_id in enumerate(stars['AGASC_ID']):
-        if agasc_id in near_ids:
-            ok[idx] = True
+    # Filter mags
+    ok = stars['MAG_ACA'] - 3.0 * stars['MAG_ACA_ERR'] / 100.0 < 11.5
 
-# Filter down to miniagasc stars
-print('Filtering from {} to {} stars'.format(len(stars), np.count_nonzero(ok)))
-stars = stars[ok]
+    # Put back near-neighbor stars that got cut by above mag filter. This file
+    # is made with create_near_neighbor_ids.py.
+    if args.proseco or not args.ignore_near_neighbors:
+        near_file = 'near_neighbor_ids_{}.fits.gz'.format(args.version)
+        near_table = Table.read(near_file, format='fits')
+        near_ids = set(near_table['near_id'])
+        print("Including {} near neighbor stars".format(len(near_ids)))
+        for idx, agasc_id in enumerate(stars['AGASC_ID']):
+            if agasc_id in near_ids:
+                ok[idx] = True
 
-if args.proseco:
-    print('Excluding columns not needed for proseco')
-    excludes = ['PLX', 'PLX_ERR', 'PLX_CATID',
-                'ACQQ1', 'ACQQ2', 'ACQQ3', 'ACQQ4', 'ACQQ5', 'ACQQ6',
-                'XREF_ID1', 'XREF_ID2', 'XREF_ID3', 'XREF_ID4', 'XREF_ID5',
-                'RSV4', 'RSV5', 'RSV6',
-                'POS_CATID', 'PM_CATID',
-                'MAG', 'MAG_ERR', 'MAG_BAND', 'MAG_CATID',
-                'COLOR1_ERR', 'C1_CATID',  # Keep color1, 2, 3
-                'COLOR2_ERR', 'C2_CATID',
-                'RSV2',
-                'VAR_CATID']
+    # Filter down to miniagasc stars
+    print('Filtering from {} to {} stars'.format(len(stars), np.count_nonzero(ok)))
+    stars = stars[ok]
 
-    names = [name for name in stars.dtype.names if name not in excludes]
-    print('Dtype before excluding:\n', stars.dtype)
-    stars = Table([stars[name] for name in names], names=names, copy=False)
-    stars = stars.as_array()
-    print('Dtype after excluding:\n', stars.dtype)
+    if args.proseco:
+        print('Excluding columns not needed for proseco')
+        excludes = ['PLX', 'PLX_ERR', 'PLX_CATID',
+                    'ACQQ1', 'ACQQ2', 'ACQQ3', 'ACQQ4', 'ACQQ5', 'ACQQ6',
+                    'XREF_ID1', 'XREF_ID2', 'XREF_ID3', 'XREF_ID4', 'XREF_ID5',
+                    'RSV4', 'RSV5', 'RSV6',
+                    'POS_CATID', 'PM_CATID',
+                    'MAG', 'MAG_ERR', 'MAG_BAND', 'MAG_CATID',
+                    'COLOR1_ERR', 'C1_CATID',  # Keep color1, 2, 3
+                    'COLOR2_ERR', 'C2_CATID',
+                    'RSV2',
+                    'VAR_CATID']
 
-print('Sorting on Dec and re-ordering')
-idx = np.argsort(stars['DEC'])
-stars = stars.take(idx)
+        names = [name for name in stars.dtype.names if name not in excludes]
+        print('Dtype before excluding:\n', stars.dtype)
+        stars = Table([stars[name] for name in names], names=names, copy=False)
+        stars = stars.as_array()
+        print('Dtype after excluding:\n', stars.dtype)
 
-print('Creating miniagasc.h5 file')
-rootname = 'proseco_agasc' if args.proseco else 'miniagasc'
-filename = '{}_{}.h5'.format(rootname, args.version)
+    print('Sorting on Dec and re-ordering')
+    idx = np.argsort(stars['DEC'])
+    stars = stars.take(idx)
 
-table_desc, bo = tables.descr_from_dtype(stars.dtype)
-minih5 = tables.open_file(filename, mode='w')
-minitbl = minih5.create_table('/', 'data', table_desc,
-                              title='AGASC {}'.format(num_version))
-print('Appending stars to {} file'.format(filename))
-minitbl.append(stars)
-minitbl.flush()
+    print('Creating miniagasc.h5 file')
+    rootname = 'proseco_agasc' if args.proseco else 'miniagasc'
+    filename = '{}_{}.h5'.format(rootname, args.version)
 
-print('Creating indexes in miniagasc.h5 file')
-if not args.proseco:
-    minitbl.cols.RA.create_csindex()
-    minitbl.cols.DEC.create_csindex()
-minitbl.cols.AGASC_ID.create_csindex()
+    table_desc, bo = tables.descr_from_dtype(stars.dtype)
+    minih5 = tables.open_file(filename, mode='w')
+    minitbl = minih5.create_table('/', 'data', table_desc,
+                                title='AGASC {}'.format(num_version))
+    print('Appending stars to {} file'.format(filename))
+    minitbl.append(stars)
+    minitbl.flush()
 
-print('Flush and close miniagasc.h5 file')
-minitbl.flush()
-minih5.flush()
-minih5.close()
+    print('Creating indexes in miniagasc.h5 file')
+    if not args.proseco:
+        minitbl.cols.RA.create_csindex()
+        minitbl.cols.DEC.create_csindex()
+    minitbl.cols.AGASC_ID.create_csindex()
+
+    print('Flush and close miniagasc.h5 file')
+    minitbl.flush()
+    minih5.flush()
+    minih5.close()
+
+
+if __name__ == '__main__':
+    main()
