45b1533f9c288849fd0419bbf66b602c4ce59da2
Tom Aldcroft <taldcroft@gmail.com>
2023-09-13 13:00:59 -0400

    Improvements to miniagasc creation helper scripts

--- create_mini_agasc_h5.py
+++ create_mini_agasc_h5.py
@@ -1,29 +1,51 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
+"""
+create_mini_agasc_h5
+====================
 
+This script creates a mini AGASC HDF5 file from the full AGASC catalog.
+
+The output can be either ``miniagasc_<version>.h5`` or ``proseco_agasc_<version>.h5``.
+
+Examples
+--------
+
+Most common usage::
+
+  $ python create_mini_agasc_h5.py --version 1.8 --include-near-neighbors
+  $ python create_mini_agasc_h5.py --version 1.8 --proseco
+"""
 import argparse
-import re
 
 import numpy as np
 import tables
 from astropy.table import Table
+
+from agasc.healpix import get_healpix
 
 
 def get_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--version",
-        default="1p7",
-        help="Version (e.g. 1p6 or 1p7, default=1p7",
+        type=str,
+        required=True,
+        help="Version (e.g. 1.7 or 1.8rc3)",
     )
     parser.add_argument(
-        "--ignore-near-neighbors",
+        "--include-near-neighbors",
         action="store_true",
-        help="Ignore near neighbor stars",
+        help="Include near neighbor stars even if filtered out by magnitude",
     )
     parser.add_argument(
         "--proseco",
         action="store_true",
-        help="Create proseco_agasc",
+        help="Create proseco_agasc (this implies --include-near-neighbors)",
+    )
+    parser.add_argument(
+        "--healpix",
+        action="store_true",
+        help="Create AGASC file using HEALpix ordering with a healpix_index table",
     )
     return parser
 
@@ -32,41 +54,73 @@ def main():
     parser = get_parser()
     args = parser.parse_args()
 
-    num_version = re.sub(r"p", ".", args.version)
-    stars = get_full_agasc_stars(args.version, args.proseco, args.ignore_near_neighbors)
+    version_num = args.version
+    version = version_num.replace(".", "p")
+    rootname = "proseco_agasc" if args.proseco else "miniagasc"
+
+    filename_full = f"agasc{version}.h5"
+    filename_mini = f"{rootname}_{args.version}.h5"
+
+    stars = get_mini_agasc_stars(
+        filename_full,
+        version,
+        include_near_neighbors=args.include_near_neighbors or args.proseco,
+    )
 
     if args.proseco:
         stars = filter_proseco_stars(stars)
 
-    print("Sorting on Dec and re-ordering")
-    idx = np.argsort(stars["DEC"])
-    stars = stars.take(idx)
+    if args.healpix:
+        print("Creating healpix_index table and sorting by healpix index")
+        healpix_index, idx_sort = get_healpix_index_table(stars)
+    else:
+        print("Sorting on DEC column")
+        idx_sort = np.argsort(stars["DEC"])
+    stars = stars.take(idx_sort)
 
-    write_mini_agasc(args, num_version, stars)
+    write_mini_agasc(args, version_num, stars)
+    if args.healpix:
+        write_healpix_index_table(filename_mini, healpix_index)
 
 
-def write_mini_agasc(args, num_version, stars):
-    print("Creating miniagasc.h5 file")
-    rootname = "proseco_agasc" if args.proseco else "miniagasc"
-    filename = f"{rootname}_{args.version}.h5"
+def write_healpix_index_table(filename: str, healpix_index: Table, nside: int):
+    """
+    Write a HEALPix index table to an HDF5 file.
 
-    table_desc, bo = tables.descr_from_dtype(stars.dtype)
-    minih5 = tables.open_file(filename, mode="w")
-    minitbl = minih5.create_table("/", "data", table_desc, title=f"AGASC {num_version}")
-    print(f"Appending stars to {filename} file")
-    minitbl.append(stars)
-    minitbl.flush()
+    Parameters
+    ----------
+    filename : str
+        The path to the HDF5 file to write to.
+    healpix_index : astropy.table.Table
+        The HEALPix index table to write.
+    nside : int
+        The NSIDE parameter used to generate the HEALPix index.
 
-    print("Creating indexes in miniagasc.h5 file")
-    if not args.proseco:
-        minitbl.cols.RA.create_csindex()
-        minitbl.cols.DEC.create_csindex()
-    minitbl.cols.AGASC_ID.create_csindex()
+    Returns
+    -------
+    None
+    """
+    healpix_index_np = healpix_index.as_array()
 
-    print("Flush and close miniagasc.h5 file")
-    minitbl.flush()
-    minih5.flush()
-    minih5.close()
+    with tables.open_file(filename, mode="a") as h5:
+        h5.create_table("/", "healpix_index", healpix_index_np, title="HEALPix index")
+        h5.root.healpix_index.attrs["nside"] = nside
+
+
+def write_mini_agasc(filename: str, stars: np.ndarray, version_num: str):
+    print(f"Creating {filename}")
+
+    with tables.open_file(filename, mode="w") as h5:
+        data = h5.create_table("/", "data", stars, title=f"AGASC {version_num}")
+        data.attrs["version"] = version_num
+        data.flush()
+
+        print("  Creating AGASC_ID index")
+        data.cols.AGASC_ID.create_csindex()
+
+        print(f"  Flush and close {filename}")
+        data.flush()
+        h5.flush()
 
 
 def filter_proseco_stars(stars):
@@ -93,12 +147,31 @@ def filter_proseco_stars(stars):
     return stars
 
 
-def get_full_agasc_stars(
-    version: str,
-    proseco: bool,
-    ignore_near_neighbors: bool,
+def get_mini_agasc_stars(
+    filename: str,
+    version_str: str,
+    include_near_neighbors: bool,
 ) -> np.ndarray:
-    filename = f"agasc{version}.h5"
+    """
+    Reads the full AGASC data from the given file and selects the usable stars based on
+    their magnitude. If `proseco` is True or `include_near_neighbors` is False, it
+    includes the near-neighbor stars that got cut by the magnitude filter. Filters down
+    to miniagasc stars and returns the resulting structured ndarray.
+
+    Parameters:
+    -----------
+    filename : str
+        The path to the file containing the full AGASC data.
+    version_str : str
+        The version string to use for the near-neighbor file.
+    include_near_neighbors : bool
+        If True, include the near-neighbor stars that got cut by the magnitude filter.
+
+    Returns:
+    --------
+    np.ndarray
+        The resulting array of miniagasc stars.
+    """
     print(f"Reading full AGASC {filename} and selecting useable stars")
 
     with tables.open_file(filename) as h5:
@@ -109,8 +182,8 @@ def get_full_agasc_stars(
 
     # Put back near-neighbor stars that got cut by above mag filter. This file
     # is made with create_near_neighbor_ids.py.
-    if proseco or not ignore_near_neighbors:
-        near_file = f"near_neighbor_ids_{version}.fits.gz"
+    if proseco or include_near_neighbors:
+        near_file = f"near_neighbor_ids_{version_str}.fits.gz"
         near_table = Table.read(near_file, format="fits")
         near_ids = set(near_table["near_id"])
         print(f"Including {len(near_ids)} near neighbor stars")
@@ -123,6 +196,42 @@ def get_full_agasc_stars(
     stars = stars[ok]
 
     return stars
+
+
+def get_healpix_index_table(stars, nside) -> tuple[Table, np.ndarray]:
+    """Return table that maps healpix index to row ranges in ``stars``.
+
+    Parameters
+    ----------
+    stars : astropy.table.Table
+        A table of stars with columns "RA" and "DEC" in degrees.
+    nside : int
+        The nside parameter of the HEALPix grid to use.
+
+    Returns
+    -------
+    healpix_index : astropy.table.Table
+        A table with columns "healpix", "row0", and "row1". The "healpix" column
+        contains the healpix index for each group of rows, and the "row0" and "row1"
+        columns contain the starting and ending row indices in `stars` for each group.
+    idxs_sort : numpy.ndarray
+        The indices that sort ``stars`` by healpix index.
+    """
+    hp = get_healpix(nside)
+
+    idxs_healpix = hp.lonlat_to_healpix(stars["RA"] * u.deg, stars["DEC"] * u.deg)
+    idxs_sort = np.argsort(idxs_healpix)
+    idxs_healpix_sort = idxs_healpix[idxs_sort]
+
+    # Make an index table for the positions where idxs_healpix_sort changes.
+    # This is the start of each healpix.
+    i0 = np.flatnonzero(np.diff(idxs_healpix_sort) != 0)
+    i0s = np.concatenate([[0], i0 + 1])
+    i1s = np.concatenate([i0 + 1, [len(idxs_healpix_sort)]])
+    healpix_index = Table(
+        [idxs_healpix_sort[i0s], i0s, i1s], names=["healpix", "row0", "row1"]
+    )
+    return healpix_index, idxs_sort
 
 
 if __name__ == "__main__":
--- create_near_neighbor_ids.py
+++ create_near_neighbor_ids.py
@@ -10,44 +10,65 @@ of AGASC 1.7 on a local (fast) drive.
 
 Usage::
 
-  $ python make_near_neighbor_ids.py
+  $ python make_near_neighbor_ids.py --version=1.7
 """
-import tables
+import argparse
 import os
 from pathlib import Path
 
-import agasc
+import tables
+import tqdm
 from astropy.table import Table
 
-agasc1p7 = str(Path(os.environ["SKA"], "data", "agasc", "agasc1p7.h5"))
+import agasc
 
-h5 = tables.open_file(agasc1p7)
-stars = h5.root.data[:]
-h5.close()
 
-ok = (
-    (stars["CLASS"] == 0)
-    & (stars["MAG_ACA"] < 11.0)
-    & (stars["ASPQ1"] < 50)
-    & (stars["ASPQ1"] > 0)  # Less than 2.5 arcsec from nearby star
-    & (stars["ASPQ2"] == 0)  # Proper motion less than 0.5 arcsec/yr
-)
-
-# Candidate acq/guide stars with a near neighbor that made ASPQ1 > 0
-nears = stars[ok]
-
-radius = 60 / 3600
-near_ids = set()
-for ii, sp in enumerate(nears):
-    near = agasc.get_agasc_cone(
-        sp["RA"], sp["DEC"], radius=radius, date="2000:001", agasc_file=agasc1p7
+def get_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--version",
+        type=str,
+        required=True,
+        help="Version (e.g. 1.7 or 1.8rc3)",
     )
-    for id in near["AGASC_ID"]:
-        if id != sp["AGASC_ID"]:
-            near_ids.add(id)
+    return parser
 
-    if ii % 100 == 0:
-        print(ii)
 
-t = Table([list(near_ids)], names=["near_id"])
-t.write("near_neighbor_ids_1p7.fits.gz", format="fits")
+def main():
+    parser = get_parser()
+    args = parser.parse_args()
+
+    version = args.version
+
+    agasc_full = str(Path(os.environ["SKA"], "data", "agasc", f"agasc{version}.h5"))
+
+    with tables.open_file(agasc_full) as h5:
+        stars = h5.root.data[:]
+
+    ok = (
+        (stars["CLASS"] == 0)
+        & (stars["MAG_ACA"] < 11.0)
+        & (stars["ASPQ1"] < 50)
+        & (stars["ASPQ1"] > 0)  # Less than 2.5 arcsec from nearby star
+        & (stars["ASPQ2"] == 0)  # Proper motion less than 0.5 arcsec/yr
+    )
+
+    # Candidate acq/guide stars with a near neighbor that made ASPQ1 > 0
+    nears = stars[ok]
+
+    radius = 60 / 3600
+    near_ids = set()
+    for sp in tqdm.tqdm(nears):
+        near = agasc.get_agasc_cone(
+            sp["RA"], sp["DEC"], radius=radius, date="2000:001", agasc_file=agasc_full
+        )
+        for id in near["AGASC_ID"]:
+            if id != sp["AGASC_ID"]:
+                near_ids.add(id)
+
+    t = Table([list(near_ids)], names=["near_id"])
+    t.write(f"near_neighbor_ids_{version}.fits.gz", format="fits")
+
+
+if __name__ == "__main__":
+    main()
