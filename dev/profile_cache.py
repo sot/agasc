20b6fb460cacf3a2ddb5897ffa0c80e4fdd440dd
Tom Aldcroft <taldcroft@gmail.com>
2024-07-05 16:25:49 -0400

    Also test columns in cache profiling

--- dev/profile_cache.py
+++ dev/profile_cache.py
@@ -1,31 +1,56 @@
 """Profile performance of get_agasc_cone with and without cache"""
 
+import itertools
 import time
 from pathlib import Path
 
 import numpy as np
 
-from agasc import get_agasc_cone, get_agasc_filename
+from agasc import __version__, get_agasc_cone, get_agasc_filename
+
+print(f"agasc version: {__version__}")
 
 agasc_healpix = get_agasc_filename("proseco_agasc_*", version="1p8", allow_rc=True)
 agasc_dec = get_agasc_filename("proseco_agasc_*", version="1p7")
 
-ras = np.random.uniform(0, 360, 100)
-decs = np.random.uniform(-90, 90, 100)
+n_iter = 300
+np.random.seed(0)
+ras = np.random.uniform(0, 360, n_iter)
+decs = np.random.uniform(-90, 90, n_iter)
+
+colnames = (
+    "AGASC_ID",
+    "RA",
+    "DEC",
+    "PM_RA",
+    "PM_DEC",
+    "EPOCH",
+    "MAG_ACA",
+)
 
 
 def print_timing(filename, cache):
-    t0 = time.time()
-    for ra, dec in zip(ras, decs):
-        get_agasc_cone(
-            ra,
-            dec,
-            radius=1.5,
-            agasc_file=filename,
-            date="2019:001",
-            cache=cache,
+    t0 = 0
+    for cols in (None, colnames):
+        for idx, ra, dec in zip(itertools.count(), ras, decs):
+            get_agasc_cone(
+                ra,
+                dec,
+                radius=1.5,
+                agasc_file=filename,
+                date="2019:001",
+                cache=cache,
+                columns=cols,
+                fix_color1=False,
+                use_supplement=False,
+                pm_filter=False,
+            )
+            if idx == 0:
+                t0 = time.time()
+        dt_ms = (time.time() - t0) / (len(ras) - 1) * 1000
+        print(
+            f"filename={Path(filename).name} {cache=} columns:{cols is not None} {dt_ms:.2f} ms"
         )
-    print(f"filename={Path(filename).name} {cache=} {time.time() - t0:.2f}")
 
 
 print_timing(agasc_dec, cache=False)
