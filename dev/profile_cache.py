c4297a152089739b698b8eca9b70a64eb5685882
Tom Aldcroft <taldcroft@gmail.com>
2024-07-07 06:13:49 -0400

    Improve profile_cache script

--- dev/profile_cache.py
+++ dev/profile_cache.py
@@ -5,10 +5,9 @@ import time
 from pathlib import Path
 
 import numpy as np
+from astropy.table import Table
 
 from agasc import __version__, get_agasc_cone, get_agasc_filename
-
-print(f"agasc version: {__version__}")
 
 agasc_healpix = get_agasc_filename("proseco_agasc_*", version="1p8", allow_rc=True)
 agasc_dec = get_agasc_filename("proseco_agasc_*", version="1p7")
@@ -29,31 +28,39 @@ colnames = (
 )
 
 
-def print_timing(filename, cache):
+def get_timing(filename, cache, cols):
     t0 = 0
-    for cols in (None, colnames):
-        for idx, ra, dec in zip(itertools.count(), ras, decs):
-            get_agasc_cone(
-                ra,
-                dec,
-                radius=1.5,
-                agasc_file=filename,
-                date="2019:001",
-                cache=cache,
-                columns=cols,
-                fix_color1=False,
-                use_supplement=False,
-                pm_filter=False,
-            )
-            if idx == 0:
-                t0 = time.time()
-        dt_ms = (time.time() - t0) / (len(ras) - 1) * 1000
-        print(
-            f"filename={Path(filename).name} {cache=} columns:{cols is not None} {dt_ms:.2f} ms"
+    for idx, ra, dec in zip(itertools.count(), ras, decs):
+        get_agasc_cone(
+            ra,
+            dec,
+            radius=1.5,
+            agasc_file=filename,
+            date="2019:001",
+            cache=cache,
+            columns=cols,
+            fix_color1=False,
+            use_supplement=False,
+            pm_filter=False,
         )
+        if idx == 0:
+            t0 = time.time()
+    dt_ms = (time.time() - t0) / (len(ras) - 1) * 1000
+    row = (Path(filename).name, cache, cols is None, dt_ms)
+    return row
 
 
-print_timing(agasc_dec, cache=False)
-print_timing(agasc_dec, cache=True)
-print_timing(agasc_healpix, cache=False)
-print_timing(agasc_healpix, cache=True)
+rows = []
+for filename in (agasc_dec, agasc_healpix):
+    for cache in (False, True):
+        for cols in (None, colnames):
+            print(".", end="", flush=True)
+            rows.append(get_timing(filename, cache, cols))
+
+out = Table(rows=rows, names=("filename", "cache", "all_columns", "dt"))
+out["dt"].format = ".2f"
+out["dt"].unit = "ms"
+
+print()
+print(f"agasc version: {__version__}")
+out.pprint_all()
